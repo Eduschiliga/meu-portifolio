8bafc6d0877d87c7896b11e089dbd8b1e6196f59
Eduardo Schiliga <eschiliga@alunos.utfpr.edu.br>
2023-08-15 14:55:41 -0300

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,3 +110,19 @@ const scrollActive = () => {
 };
 
 window.addEventListener("scroll", scrollActive);
+
+
+/* ANIMAÇÃO SCROLL REVEAL */
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 350,
+  reset: true // repetir animação
+})
+
+sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`)
+sr.reveal(`.home__image`, {origin: 'bottom'})
+sr.reveal(`.about__data, .skills__data`, {origin: 'left'})
+sr.reveal(`.about__image, .skills__content`, {origin: 'rigth'})
+sr.reveal(`.services__card, .projects__cards`, {interval: 100})
